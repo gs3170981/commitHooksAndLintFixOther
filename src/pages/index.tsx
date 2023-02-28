a1be033b7a8daa369886867fbd88776141846552
wb-gs465826 <wb-gs465826@antgroup.com>
2023-02-28 11:52:07 +0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,12 @@
 import styles from './index.less';
 
 export default function IndexPage() {
+  const a = 3;
+
+  if (a) {
+  }
+  for (var i = 0; i < 1; i++) {}
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
